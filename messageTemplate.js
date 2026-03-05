eaf9d5f39260e1c7d7aa68f223dbf63f6a0eab56
面条君 <1769899752@qq.com>
2026-03-05 23:44:43 +0800

    feat:添加模板插入

---
+++ messageTemplate.js
@@ -0,0 +1,7 @@
+import { HumanMessage } from '@langchain/core/messages';
+
+export const createHtmlMessage = ({text, fileName, skill}) => {
+    return new HumanMessage({
+        content: `你的任务是将用户输入的文本 ${text} 根据 ${skill} skill 生成html文件，文件名为 ${fileName}`,
+    });
+}
--- skill.js
+++ skill.js
@@ -1,23 +1,78 @@
-import { qwenPlus,qwenCoderPlus,qwenCoderFlash } from './model.js';
+import { qwenPlus, qwenCoderPlus, qwenCoderFlash } from './model.js';
 import { createDeepAgent, FilesystemBackend } from 'deepagents';
-import { HumanMessage, AIMessage, ToolMessage } from '@langchain/core/messages';
+import { HumanMessage, AIMessage, ToolMessage, SystemMessage } from '@langchain/core/messages';
+import { createMiddleware } from 'langchain';
+import z from 'zod';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { readFile as fsReadFile } from 'fs/promises';
+import { createHtmlMessage } from './messageTemplate.js';
+export async function readFileUtf8(filePath) {
+    const baseDir = path.dirname(fileURLToPath(import.meta.url));
+    const target = path.isAbsolute(filePath) ? filePath : path.resolve(baseDir, filePath);
+    return await fsReadFile(target, { encoding: 'utf-8' });
+}
+
+// 定义上下文schema
+const contextSchema = z.object({
+    skillId: z.string(),
+    text: z.string(),
+    fileName: z.string(),
+});
+
+// const dynamicPromptMiddleware = createMiddleware({
+//     name: "DynamicPrompt",
+//     contextSchema,
+//     beforeModel: (state, runtime) => {
+//         const { skillId, text, fileName } = runtime.context;
+//         console.log(skillId, text, fileName, 'skillId, text, fileName');
+//         const systemPrompt = `
+// 你是一个文本生成助手
+
+// 你的任务是将用户输入的文本 ${text} 根据用户指定的skill ${skillId} 生成html文件，文件名为 ${fileName}
+
+// 你可以使用的skill如下:
+// 1. web-design-guidelines: 生成符合web设计指南的html文本
+// 2. pixel-art-css: 生成符合像素艺术风格的css
+// 3. modern-css: 生成modern风格的css
+
+// 阅读技能或长文件时，请按以下策略操作，确保拿到完整内容：
+// - 优先使用 read_file 分页读取（例如 limit=500），并通过调整 offset 连续读取直到文件末尾
+// - 如果工具提示结果过大被截断或被写回文件系统，立即改为按页继续读取原文件
+// - 只读取与当前任务相关的部分，减少一次性读取长度，避免再次截断
+// `
+//         return {
+//             systemMessage: new SystemMessage(systemPrompt),
+//         };
+//     },
+// });
 
 const agent = createDeepAgent({
     model: qwenPlus,
-    systemPrompt: `你是一个文本生成助手，你可以使用的skill如下:
+    systemPrompt: `
+    你是一个文本生成助手
+    你可以使用的skill如下:
     1. web-design-guidelines: 生成符合web设计指南的html文本
     2. pixel-art-css: 生成符合像素艺术风格的css
     3. modern-css: 生成modern风格的css
+
+    生成任务完成后不要进行解释，不需要进行概况总结，直接结束任务，只执行用户输入的任务，不进行任何解释。
     
     阅读技能或长文件时，请按以下策略操作，确保拿到完整内容：
     - 优先使用 read_file 分页读取（例如 limit=500），并通过调整 offset 连续读取直到文件末尾
     - 如果工具提示结果过大被截断或被写回文件系统，立即改为按页继续读取原文件
     - 只读取与当前任务相关的部分，减少一次性读取长度，避免再次截断
     `,
+    // interruptOn: {
+    //     write_file: true,
+    //     edit_file: true,
+    //     delete_file: false,
+    //     read_file: true,
+    // },
     backend: new FilesystemBackend({ rootDir: path.dirname(fileURLToPath(import.meta.url)), virtualMode: true }),
-    skills: ['/skills/']
+    skills: ['/skills/'],
+    // middleware: [dynamicPromptMiddleware],
+    // contextSchema,
 });
 
 const render = async () => {
@@ -34,14 +89,18 @@ const render = async () => {
             usageTotals.total += t;
         }
     }];
+    const resume = await readFileUtf8('虚拟人物简历.md');
     const result = await agent.stream({
         messages: [
-            new HumanMessage({
-                content: `读取 虚拟人物简历.md 根据其内容生成一份html简历,不要额外添加任何信息,
-                文件命名为test10.html如果已有该文件则test后序号递增,严格按照web设计指南生成,要注意文字大小以及排版合理,使用html-render 技能来指导生成`,
+            createHtmlMessage({
+                text: resume,
+                fileName: 'test14.html',
+                skill: 'html-render',
             }),
         ]
-    }, { streamMode: 'values', callbacks });
+    }, {
+        streamMode: 'values', callbacks
+    });
     for await (const chunk of result) {
         for (const message of chunk.messages) {
             console.log('--------------ALL START---------------')
@@ -66,8 +125,7 @@ const render = async () => {
     console.log(`Token 消耗: 输入 ${usageTotals.prompt} + 输出 ${usageTotals.completion} = 总计 ${usageTotals.total}`);
 }
 try {
-render();
-    
+    render();
 } catch (error) {
     console.error('出错:', error);
 }
